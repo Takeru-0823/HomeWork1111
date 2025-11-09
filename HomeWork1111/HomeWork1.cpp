8ab5e1f62f176e9199ec24ebc65fb71b0f56d7ce
buffa <buffaloes0534@icloud.com>
2025-11-09 10:09:58 +0900

    プロジェクト ファイルを追加します。

---
+++ HomeWork1111/HomeWork1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void swap_string(char* s1, char* s2);
+
+int main()
+{
+    char p[] = "ABCDEF";
+    char q[] = "XYZ";
+
+    swap_string(p, q);
+
+    cout << p << endl;
+}
+
+void swap_string(char* s1, char* s2)
+{
+
+}
+
---
+++ HomeWork1111/HomeWork2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int str2int(const char* s);
+
+int main()
+{
+	char s[] = "138";
+
+	int answer = str2int(s);
+
+	cout << "“š‚¦‚Í " << answer << endl;
+}
+
+int str2int(const char* s)
+{
+	int answer = 0;
+	int index1 = 0;
+	int index2 = 0;
+	int disit = 1;
+	char number[] = "0123456789";
+
+	while (s[index1] != '\0')
+	{
+		if (s[index1] == number[index2])
+		{
+			answer *= disit;
+			answer += index2;
+			index1++;
+			index2 = 0;
+
+			if (disit == 1)
+			{
+				disit = 10;
+			}
+		}
+		else
+		{
+			index2++;
+		}
+
+		if (number[index2] == '\0')
+		{
+			return 0;
+		}
+	}
+
+	return answer;
+
+}
